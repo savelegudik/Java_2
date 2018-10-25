e1f5e4858460f5e2398bd203200255ade6efcbe1
savelegudik <savelegudik1@gmail.com>
2018-10-25 21:45:11 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -8,59 +8,57 @@ public class Main {
     }
 
     public static void chooseAction(ArrayList arr) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1 - add; 2 - remove; 3 - show list");
-        int chosenNumber = sc.nextInt();
-        switch (chosenNumber) {
-            case 1:
-                add(arr);
-                returnProgram(arr);
-                break;
-            case 2:
-                remove(arr);
-                returnProgram(arr);
-                break;
-            case 3:
-                showList(arr);
-                returnProgram(arr);
-                break;
-            default:
-                System.out.println("Error");
-                returnProgram(arr);
-                break;
+        while (true) {
+            showMenu();
+            int menuItem = getFromUserMenuToExecute();
+            switch (menuItem) {
+                case 1:
+                    addToList(arr);
+                    break;
+                case 2:
+                    removeFromList(arr);
+                    break;
+                case 3:
+                    showListToConsole(arr);
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
         }
     }
 
-    public static ArrayList add(ArrayList arr) {
+    public static void addToList(ArrayList arr) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Write to add to list: ");
+        System.out.println("Write title to add to list: ");
         arr.add(sc.nextLine());
-        System.out.println(arr);
         System.out.println();
-        return arr;
     }
 
-    public static ArrayList remove(ArrayList arr) {
+    public static void removeFromList(ArrayList arr) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Write to remove to list: ");
+        System.out.println("Write title to remove from list: ");
         arr.remove(sc.nextLine());
-        System.out.println(arr);
         System.out.println();
-        return arr;
     }
 
-    public static ArrayList showList(ArrayList arr) {
+    public static void showListToConsole(ArrayList arr) {
         System.out.println(arr);
         System.out.println();
-        return arr;
     }
 
-    public static void returnProgram(ArrayList arr) {
+    public static void showMenu() {
+        System.out.println("Program Menu:");
+        System.out.println("1 - Add to list");
+        System.out.println("2 - Remove from list");
+        System.out.println("3 - Show list");
+        System.out.println("4 - Exit from program");
+        System.out.println();
+    }
+
+    public static int getFromUserMenuToExecute() {
+        System.out.println("Enter menu item number: ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Do you want to restart a program? ");
-        String decision = sc.nextLine();
-        if (decision.equals("y")) {
-            chooseAction(arr);
-        }
+        return Integer.parseInt(sc.nextLine());
     }
 }
